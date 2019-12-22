197f58678ef0d5124ba5ddaa734af94c4b21b5e8
Syed Mohammad Hassan <hassan.imroze@hotmail.com>
2019-12-22 23:38:36 +0600

    Add project files.

---
+++ DS-Arrays/GenericArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Arrays
+{
+    public class GenericArray<T>
+    {
+        private T[] array;
+
+        public GenericArray(int length)
+        {
+            array = new T[length];
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+    }
+}
---
+++ DS-Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicArrayPrint();
+            Console.WriteLine();
+
+            ArrayClassExcercise();
+
+            Console.ReadKey();
+        }
+
+        static void BasicArrayPrint()
+        {
+            int[] array = new int[3];
+            array[2] = 11;
+            Console.WriteLine("Printing all items");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(string.Format("{0},",array[i]));
+            }
+
+        }
+
+        static void ArrayClassExcercise()
+        {
+            GenericArray<int> numbers = new GenericArray<int>(length: 3);
+            numbers.Print();
+        }
+    }
+}
